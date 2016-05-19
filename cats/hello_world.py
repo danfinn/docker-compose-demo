6f7df38c313c3277bb13a68ffc8a36401317bbee
Dan Finn <dan_j_finn@yahoo.com>
2016-05-19 06:15:28 +0000

    checking in

--- cats/hello_world.py
+++ cats/hello_world.py
@@ -1,9 +1,16 @@
 from flask import Flask
+from pymongo import MongoClient
+client = MongoClient('mongodb', 27017)
+db = client.data
+collection = db.zips
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Flask Dockerized'
+
+def get_random_zip()
+    zip = collection.aggregate( { $sample: { size: 1 } } )
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
