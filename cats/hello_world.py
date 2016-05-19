2b41f365eb0f951a19b3de2d5e8e3c1e757e0881
Dan Finn <dan_j_finn@yahoo.com>
2016-05-19 20:58:36 +0000

    seeding db, app is pulling random city from the DB and displaying it via http.

--- cats/hello_world.py
+++ cats/hello_world.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from pymongo import MongoClient
+import random
+
 client = MongoClient('mongodb', 27017)
 db = client.data
 collection = db.zips
@@ -7,10 +9,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Flask Dockerized'
+    zip = get_random_zip()
+    #print zip['city']
+    return zip['city']
 
-def get_random_zip()
-    zip = collection.aggregate( { $sample: { size: 1 } } )
+def get_random_zip():
+    count = collection.count()
+    return collection.find()[random.randrange(count)]  
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
