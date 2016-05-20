173f916ebf676721162ec90920f52c52ea093307
Dan Finn <dan_j_finn@yahoo.com>
2016-05-20 06:45:34 +0000

    working, now need to make it pretty'er

--- cats/hello_world.py
+++ cats/hello_world.py
@@ -1,21 +1,38 @@
 from flask import Flask
 from pymongo import MongoClient
 import random
+import redis
 
 client = MongoClient('mongodb', 27017)
 db = client.data
 collection = db.zips
 app = Flask(__name__)
 
+r = redis.Redis(host='redis')
+r.set('counter','0')
+
 @app.route('/')
 def hello_world():
+    increment_redis()
     zip = get_random_zip()
     #print zip['city']
     return zip['city']
 
+@app.route('/counter')
+def counter():
+    return get_redis()
+
 def get_random_zip():
     count = collection.count()
     return collection.find()[random.randrange(count)]  
+
+def increment_redis():
+    r.incr('counter',1)
+    return
+
+def get_redis():
+    value=r.get('counter')
+    return value
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
